85147f43342a788fdc664123eab789f9b33e20dc
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-15 00:35:59 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+# require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, hash|
+    hash.each do |k, arr|
+      arr.each do |name|
+        if !pigeon_list.include?(name)
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name].include?(k)
+          pigeon_list[name][key] = [k.to_s]
+        end
+      end
+    end
+  end
+  # data.each do |key, hash|
+  #   hash.each do |k, arr|
+  #     arr.each do |name|
+  #
+  #     end
+  #   end
+  # end
+
+  # binding.pry
+  return pigeon_list
 end
