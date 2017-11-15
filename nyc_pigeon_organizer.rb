286a3977c26aefe2d1cf7cfc6329b32ebbde3ed4
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-15 01:14:50 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each do |key, hash|
     hash.each do |k, arr|
       arr.each do |name|
-
+        if !pigeon_list.include?(name)
           pigeon_list[name] = {}
-
+        end
         if !pigeon_list[name].include?(k)
           pigeon_list[name][key] = [k.to_s]
         else
