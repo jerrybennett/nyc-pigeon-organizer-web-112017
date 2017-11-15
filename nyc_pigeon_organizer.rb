6500c4c7b0db994afe21106637d8cdd9fdf2af73
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-15 01:23:33 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
         if !pigeon_list[name].include?(k)
           pigeon_list[name][key] = [k.to_s]
         end
-        arr.select{|i| pigeon_list[name][key] << i }
+        key.select{|i| pigeon_list[name][key] << i }
       end
     end
   end
